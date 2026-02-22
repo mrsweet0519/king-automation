55829f3faf6d44752c19fc1fabc0bae5235810ef
mrsweet0519 <mrsweet0519@gmail.com>
2026-02-23 07:30:49 +0900

    Add files via upload

---
+++ 결과물/고객관리/synced_data.js
@@ -0,0 +1,20 @@
+window.CRM_SYNC_DATA = [
+  {
+    "id": 1771768134528,
+    "name": "동양트레이딩.임미례",
+    "phone": "",
+    "path": "모바일(자동)",
+    "status": "interest",
+    "memo": "djwlwkdj",
+    "date": "2026-02-22"
+  },
+  {
+    "id": 1771768134529,
+    "name": "동양트레이딩.정대표",
+    "phone": "",
+    "path": "모바일(자동)",
+    "status": "interest",
+    "memo": "Test 신호 수신 성공!",
+    "date": "2026-02-22"
+  }
+];
